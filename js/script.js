b83efeb85f341315473174e23aeea6f4d38b347a
Christian Pajares <christian.pajares.contractor@bbva.com>
2021-10-12 22:48:55 -0500

    add features

--- js/script.js
+++ js/script.js
@@ -26,3 +26,9 @@ for(let i= 0; i < inputs.length; i++){
         }
     });
 }
+
+//map
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
